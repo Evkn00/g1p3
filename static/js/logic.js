b045b545d6514d949740003c5ce514d0b1897180
wickedwes78 <127099343+wickedwes78@users.noreply.github.com>
2023-07-25 23:08:15 +0930

    Update logic.js
    Update for duplicating legends and layer controls

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,16 +2,20 @@
   //set default years
   let startYear = 1
   let endYear = 2030
-  let markerLayer;
+  let markerLayer
+  let legend
 
   // Create data URL
   let url = `http://127.0.0.1:5000/documents/${startYear}/${endYear}`;// URL for Flask API
 
-// Initialize the map
+  // Initialize the map
   let myMap = L.map("map", {
     center: [-30.8, 130.9],
     zoom: 5
   });
+
+  // Create layer control outside the makeMap function
+  const layerControl = L.control.layers(baseLayers).addTo(myMap);  
 
 function makeMap(xxx) {  
   d3.json(xxx)
@@ -20,9 +24,6 @@ function makeMap(xxx) {
       console.log(data);
 
       defaultTileLayer.addTo(myMap);
-      
-      // Create layer control
-      L.control.layers(baseLayers).addTo(myMap);
       
       // Generate summary stats
       // Number of records
@@ -71,10 +72,13 @@ function makeMap(xxx) {
       console.log("LOSSDATE Array:", lossDateArr);
 
       // Initialize the marker layer with all ships
-      let markerLayer = createMarkerLayer(data);
+      markerLayer = createMarkerLayer(data);
 
     // Add the marker layer to the map
     markerLayer.addTo(myMap);
+
+    // Update the legend content
+    updateLegendContent();
 
     // Get unique RIGDESC values
     var rigDescValues = data.map(function(feature) {
@@ -82,7 +86,6 @@ function makeMap(xxx) {
     return markerLayer;
     });
   
-
 
     // Function to update the map with the selected ship type
     function updateMap(selectedRigDesc) {
@@ -126,14 +129,9 @@ window.populateStoryBox = function(properties) {
   };
 
 
-
-
-// Create a legend control.
-let legend = L.control({ position: 'bottomright' });
-
 function createLegend() {
     // Create a legend control.
-    let legend = L.control({ position: 'bottomright' });
+    legend = L.control({ position: 'bottomright' });
   
     legend.onAdd = function (map) {
       var div = L.DomUtil.create('div', 'info legend');
@@ -209,7 +207,6 @@ let minYear = 1837
 let maxYear = 2002
 
 
-
 // Set up the slider
 noUiSlider.create(slider, {
   start: [minYear, maxYear], // These values should be defined beforehand
@@ -234,6 +231,25 @@ slider.noUiSlider.on("update", function (values) {
   endYear = newMaxYear;
 }); 
 
+// Function to update the legend content
+function updateLegendContent() {
+  if (legend) {
+    // Create the legend content
+    let div = L.DomUtil.create("div", "info legend");
+    div.innerHTML += "<h4>Ships</h4>";
+    ships = ["All", "Barque", "Brig", "Brigantine", "Cutter", "Dandy", "Ketch", "Lugger", "Schooner", "Ship", "Sloop", "Snow", "Yawl"];
+    labels = ["static/images/ship.png", "static/images/barque.png", "static/images/Brig.png", "static/images/brigantine.png", "static/images/cutter.png", "static/images/dandy.png", "static/images/Ketch.png", "static/images/lugger.png", "static/images/Schooner.png", "static/images/ship.png", "static/images/Sloop.png", "static/images/Snow.png", "static/images/yawl.png"];
+
+    // loop through our ships and generate a label with their png for each ship
+    for (var i = 0; i < ships.length; i++) {
+      div.innerHTML += `<img src="${labels[i]}" height="20" width="20" onclick="filterByShipType('${ships[i]}')">${ships[i]}<br>`;
+    }
+
+    // Update the content of the existing legend control
+    legend.getContainer().innerHTML = div.innerHTML;
+  }
+}
+
 // Function to clear the map and recreate it
 function refreshMap() {
   // Remove the existing marker layer from the map
@@ -244,7 +260,12 @@ function refreshMap() {
 
   // Remove the existing legend control from the map
   if (legend) {
-    myMap.removeControl(legend);
+    legend.remove();
+  }
+
+  // Reset the layer control
+  if (layerControl) {
+    layerControl.remove();
   }
 
   // Rebuild the data URL with the updated startYear and endYear
@@ -253,6 +274,9 @@ function refreshMap() {
 
   // Recreate the map
   markerLayer = makeMap(url);
+
+  // Add the layer control again
+  layerControl.addTo(myMap);
 }
 
 // Add event listener to the refresh button
